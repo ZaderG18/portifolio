afb592f804e9b2e9a1fbf6e4d628389024aff3d3
ZaderG18 <arthurhenriquegr818@gmail.com>
2024-07-20 01:15:03 -0300

    primeiro

---
+++ main.js
@@ -0,0 +1,7 @@
+let typed = new Typed(".texto", {
+    strings: ["Desenvolvedor Web", "Desenvolvedor Front-end", "Desenvolvedor Back-end"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
